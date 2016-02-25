33e17b1a79f77059872ad6f8422d4e9ea5fd24af
xufei <xu.fei@outlook.com>
2016-02-25 10:15:39 +0800

    control-nav

--- src/modules/portal/controllers/control-nav.js
+++ src/modules/portal/controllers/control-nav.js
@@ -1,47 +1,9 @@
 export default class ControlNavController {
 	constructor(PortalService) {
-		this.menuList = [];
-		this.menuTree = [];
-		this.menuMap = {};
-
-		this.PortalService = PortalService;
+        this.tab = "activity";
 	}
 
 	init() {
-		this.PortalService.getMenuList().then(menus => {
-			menus.forEach(menuItem => {
-				this.menuMap[menuItem.code] = menuItem;
-
-				if (!menuItem.parent) {
-					this.menuTree.push(menuItem);
-				}
-				else {
-					let parent = this.menuMap[menuItem.parent];
-					if (!parent.children) {
-						parent.children = [];
-					}
-					parent.children.push(menuItem);
-					menuItem.parent = parent;
-				}
-			});
-			
-			this.menuList = menus;
-		});
-	}
-	
-	menuClick(menu, evt) {
-		this.menuList.forEach(item => {
-			if (item == menu) {
-				menu.$expanded = !menu.$expanded;
-			}
-			else {
-				if (item != menu.parent) {
-					delete item.$expanded;
-				}
-			}
-		});
-		
-		evt.stopPropagation();
 	}
 }
 
